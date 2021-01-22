e73cdd7617ab8c2ad1bf5ebc8ddb623bbce473a4
Emanuel Medina Gomez <emedinag@users.noreply.github.com>
2021-01-22 14:55:48 -0500

    Inicializando proyecto Git LFS Backend s3

---
+++ lib/lfs/application.ex
@@ -0,0 +1,56 @@
+defmodule Lfs.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+  alias Lfs.Config.{AppConfig, ConfigHolder}
+  use Application
+  require Logger
+  def start(_type, _args) do
+    config = AppConfig.load_config()
+    in_test? = Application.fetch_env(:lfs, :in_test)
+
+    children = with_plug_server(config) ++ application_children(in_test?)
+
+    opts = [strategy: :one_for_one, name: Lfs.Supervisor]
+    h_opts = [{:timeout, 190000}, {:max_connections, 6000}]
+    :ok = :hackney_pool.start_pool(:app_pool, h_opts)
+
+    Supervisor.start_link(children, opts)
+  end
+
+  defp with_plug_server(%AppConfig{enable_server: true, http_port: port}) do
+    Logger.info("Configure Http server in port: #{inspect(port)}")
+    [
+      {
+        Plug.Cowboy,
+        scheme: :http,
+        plug: RestController,
+        options: [
+          port: port
+        ]
+      }
+    ]
+  end
+
+  defp with_plug_server(%AppConfig{enable_server: false}), do: []
+
+
+
+  def application_children({:ok, true} = _test_env) do
+    [
+      {ConfigHolder, AppConfig.load_config()},
+      {Task.Supervisor, name: Lfs.TaskSupervisor},
+    ]
+  end
+
+  def application_children(_other_env) do
+    [
+      {ConfigHolder, AppConfig.load_config()},
+      {Task.Supervisor, name: Lfs.TaskSupervisor}
+      #{Redix, {Application.get_env(:ms_auth_ex, :redis_url), [name: :redix]}},
+      #{SecretManagerAdapter, []},
+      #{RequestCipherHolder, []},
+    
+    ]
+  end
+end
